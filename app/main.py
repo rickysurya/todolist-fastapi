a7dae06a5c28240daef94980a7b24861d0a8d2b9
Ricky Surya Adiputra <rickysuryaa10@gmail.com>
2023-06-17 14:55:15 +0700

    added post request endpoint

--- app/main.py
+++ app/main.py
@@ -2,45 +2,31 @@ from fastapi import FastAPI, Path
 from pydantic import BaseModel
 from typing import Optional
 import uvicorn
+from fastapi.params import Body
 
 
 app = FastAPI()
 
 
-students = {
-    1 :{
-    "name" : "Budi",
-    "age" : "17",
-    "class" : "B"
+class Post(BaseModel):
+    title: str
+    content: str
+
+
+@app.get("/getposts")
+def get_posts():
+    return {
+        "Message": "Post retrieved successfully"
     }
-}
 
-class Student(BaseModel):
-    name: str
-    age: int
-    year: str
 
-@app.get("/chat")
-def main():
-    return {"test" : "nah"}
+@app.post("/createposts")
+def create_posts(post: dict = Body(...)):
+    print(post)
+    return {
+        "Message": "Post successfully created!"
+    }
 
-@app.get("/get-student/{student_id}")
-def get_student(student_id: int = Path(None, description="The ID of the student you want to view", gt=0, lt=2)):
-    return students[student_id]
-
-@app.get("/get-by-name/{student_id}")
-def get_student(*, student_id: int, name:Optional[str] = None, test: int):
-    for student_id in students:
-        if students[student_id]["name"] == name:
-            return students[student_id]
-    return {"Data" : "Not found"}
-
-@app.post("/create-student/{student_id}")
-def create_student(student_id: int, student: Student):
-    if student_id in student:
-        return {"Error" : "Student exists"}
-    students[student_id] = student
-    return students[student_id]
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8000, host="0.0.0.0")
